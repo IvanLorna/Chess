92a66280359a066d71654d639c2e279ea8ec8271
GrayGorilla <nbren004@ucr.edu>
2021-05-16 01:52:28 -0700

    Added NB3 White models & weights

--- Models/NB_models.py
+++ Models/NB_models.py
@@ -69,3 +69,26 @@ def trainNB2():
                 loss = tf.keras.losses.MeanSquaredError(),
                 metrics=['accuracy'])
   return model.fit(TrainBX, TrainBY, epochs = 10)
+
+def trainNW3():
+  model = models.Sequential()
+  model.add(layers.ZeroPadding2D(padding=(1,1), input_shape=(8, 8, 14)))
+  model.add(layers.Conv2D(32, (3, 3), strides = (1,1), activation='tanh'))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.Conv2D(64, (3, 3), activation='tanh'))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.Conv2D(64, (3, 3), activation='tanh'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(128,activation='softsign'))
+  model.add(layers.Reshape((1,128)))
+
+  # model.summary()
+  model.compile(optimizer='RMSprop',
+                loss = tf.keras.losses.MeanSquaredError(),
+                metrics=['accuracy'])
+  return model.fit(TrainWX, TrainWY, epochs = 10)
+  
