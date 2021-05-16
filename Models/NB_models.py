ebd1e6173c5095d7aad0fb0c9c78ff69207de51c
GrayGorilla <nbren004@ucr.edu>
2021-05-16 09:57:46 -0700

    Added NW5 Black model & weights

--- Models/NB_models.py
+++ Models/NB_models.py
@@ -188,3 +188,20 @@ def trainNW5():
                 metrics=['accuracy'])
   return model.fit(TrainWX, TrainWY, epochs = 10)
 
+def trainNB5():
+  model = models.Sequential()
+  model.add(layers.ZeroPadding2D(padding=(1,1), input_shape=(8, 8, 14)))
+  model.add(layers.Conv2D(32, (3, 3), strides = (1,1), activation='relu'))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(128,activation='softmax'))
+  model.add(layers.Reshape((1,128)))
+
+  # model.summary()
+  model.compile(optimizer='Nadam',
+                loss = tf.keras.losses.MeanSquaredLogarithmicError(),
+                metrics=['accuracy'])
+  return model.fit(TrainBX, TrainBY, epochs = 10)
