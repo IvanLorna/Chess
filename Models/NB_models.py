c0f24c687a62dc6340baa84cc7bc3376d4ee7175
GrayGorilla <nbren004@ucr.edu>
2021-05-16 00:54:31 -0700

    Added NB2 Black models & weights

--- Models/NB_models.py
+++ Models/NB_models.py
@@ -16,7 +16,6 @@ def trainNW1():
                 metrics=['accuracy'])
   return model.fit(TrainWX, TrainWY, epochs = 10)
 
-
 def trainNB1():
   model = models.Sequential()
   model.add(layers.ZeroPadding2D(padding=(1,1), input_shape=(8, 8, 14)))
@@ -31,6 +30,24 @@ def trainNB1():
 
   # model.summary()
   model.compile(optimizer='adam',
+                loss = tf.keras.losses.MeanSquaredError(),
+                metrics=['accuracy'])
+  return model.fit(TrainBX, TrainBY, epochs = 10)
+
+def trainNB2():
+  model = models.Sequential()
+  model.add(layers.ZeroPadding2D(padding=(1,1), input_shape=(8, 8, 14)))
+  model.add(layers.Conv2D(32, (3, 3), strides = (1,1), activation='relu'))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.ZeroPadding2D(padding=(1,1)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(128,activation='softmax'))
+  model.add(layers.Reshape((1,128)))
+
+  # model.summary()
+  model.compile(optimizer='Nadam',
                 loss = tf.keras.losses.MeanSquaredError(),
                 metrics=['accuracy'])
   return model.fit(TrainBX, TrainBY, epochs = 10)
