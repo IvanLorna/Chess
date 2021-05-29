60751f0831aa3833902351a60ce3571e1fb2ca64
IvanLorna <43653055+IvanLorna@users.noreply.github.com>
2021-05-28 23:10:42 -0700

    introducing Player extension for gameplay UI

---
+++ Player.py
@@ -0,0 +1,21 @@
+class Player():
+  def __init__(self, isWhite = True):
+    self.isWhite = isWhite
+    self.name = input("Please Enter Player Name: ")
+    pass
+  
+  def MakeMove(self,board):
+    moveMade = False
+    while moveMade is False:
+      moves = 'Legal Moves to make:'
+      for i in board.legal_moves:
+        moves  = moves + ' ' + str(i)
+      print(board)
+      print(moves)
+      Move = chess.Move.from_uci(input("please Enter a Legal Move: "))
+      if Move in board.legal_moves:
+        moveMade = True
+      else:
+        print('"'+ str(Move) + '" is not a legal move.' )
+    return Move
+    
